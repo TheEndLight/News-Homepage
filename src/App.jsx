40ba65beae0888a87f74a7660efcf663614ee40b
TheEndLight <laguirre4@ucol.mx>
2024-01-26 10:29:23 -0600

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <main className='px-4 pt-6'>
       <Header />
-      <div className='lg:flex lg:gap-8'>
+      <div className='lg:flex lg:gap-x-8'>
         <MainArticle />
         <NewContainer />
       </div>
